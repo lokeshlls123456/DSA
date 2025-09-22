94394de12f2be0a37fdf0bda60ae02b70324552d
Lokesh <llslokesh123456@gmail.com>
2025-09-22 23:27:20 +0530

    Create DSA Day 2.py
    day 2 DSA

---
+++ DSA Day 2.py
@@ -0,0 +1,10 @@
+# User Registration System
+
+# Taking input from the user
+age = int(input())
+name = input()
+
+# Displaying the user details
+print("User Details:")
+print(f"Name: {name}")
+print(f"Age: {age}")
