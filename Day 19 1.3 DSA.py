27d7d59ebd651f4804439132215faa6810e8d756
Lokesh <llslokesh123456@gmail.com>
2025-10-10 23:52:37 +0530

    hhhh
    hhhh

--- Day 19 1.3 DSA.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-for i in range(n):
-    for j in range(i + 1, n):
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-for row in matrix:
-    row.reverse()
-for row in matrix:
-    print(" ".join(map(str, row)))
