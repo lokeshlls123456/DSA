99f105cc5d970a58d04162aa130f2f7f79918a7e
E.Lokesh <llslokesh123456@gmail.com>
2025-12-17 21:11:02 +0530

    Create Day 87 1.2 DSA.PY

---
+++ Day 87 1.2 DSA.PY
@@ -0,0 +1,10 @@
+length = int(input())
+breadth = int(input())
+
+perimeter = 2 * (length + breadth)
+
+
+area = length * breadth
+
+print(f"The required length is {perimeter} m")
+print(f"The required area of carpet is {area} sqm")
